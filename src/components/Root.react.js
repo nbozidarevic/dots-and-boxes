cbe2bc42008bc0b97e6259c38d9dbd54599853dd
nbozidarevic <nbozidarevic@gmail.com>
2017-08-23 20:07:14 -0700

    Add debug statements prior to introducing Flux

--- src/components/Root.react.js
+++ src/components/Root.react.js
@@ -7,8 +7,17 @@
 import GameStore from '../data/GameStore';
 import React from 'react';
 
+document.debug = GameStore;
+
 export default class Root extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        {GameStore.rows}<br />
+        {GameStore.cols}<br />
+        {GameStore.currentPlayer}<br />
+        {GameStore.state}
+      </div>
+    );
   }
 }
