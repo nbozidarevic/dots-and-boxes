6b81703c27f8d4555e12906e40831e269db6ce7c
nbozidarevic <nbozidarevic@gmail.com>
2017-09-02 14:37:52 -0700

    Set min and max size

--- src/components/GameScreen.react.js
+++ src/components/GameScreen.react.js
@@ -25,6 +25,9 @@ type State = {
   };
 }
 
+const MIN_BOX_SIZE = 36;
+const MAX_BOX_SIZE = 72;
+
 class GameScreen extends React.Component<{}, State> {
   _wrapperRef: ?React$ElementRef<'div'>;
 
@@ -111,14 +114,41 @@ class GameScreen extends React.Component<{}, State> {
       left: 0,
     };
 
+    if (!wrapperDimensions) {
+      return position;
+    }
+
+    const {width, height} = wrapperDimensions;
+
     if (
       rows === 0 ||
       cols === 0 ||
-      !wrapperDimensions ||
-      wrapperDimensions.width === 0 ||
-      wrapperDimensions.height === 0
+      width === 0 ||
+      height === 0
     ) {
       return position;
+    }
+
+    const maxWidth = cols * MAX_BOX_SIZE;
+    const maxHeight = rows * MAX_BOX_SIZE;
+    if (maxWidth <= width && maxHeight <= height) {
+      return {
+        top: (height - maxHeight) / 2,
+        right: (width - maxWidth) / 2,
+        bottom: (height - maxHeight) / 2,
+        left: (width - maxWidth) / 2,
+      }
+    }
+
+    const minWidth = cols * MIN_BOX_SIZE;
+    const minHeight = rows * MIN_BOX_SIZE;
+    if (width <= minWidth || height <= minHeight) {
+      return {
+        top: 0,
+        right: (width - minWidth) / 2,
+        bottom: height - minHeight,
+        left: (width - minWidth) / 2,
+      }
     }
 
     const boardRatio = rows / cols;
@@ -127,6 +157,9 @@ class GameScreen extends React.Component<{}, State> {
     if (boardRatio > wrapperRatio) {
       position.left = position.right =
         (wrapperDimensions.width - wrapperDimensions.height / boardRatio) / 2;
+      if (wrapperDimensions.width - 2 * position.left > MAX_BOX_SIZE * cols) {
+        console.log('resize');
+      }
     } else {
       position.top = position.bottom =
         (wrapperDimensions.height - boardRatio * wrapperDimensions.width) / 2;
