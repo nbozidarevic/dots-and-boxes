857e26c3734926607f88ade53c5b945ea4639466
nbozidarevic <nbozidarevic@gmail.com>
2017-09-14 22:17:47 -0700

    Plan out the optimizing algorithm up to alpha-beta pruning

--- src/behaviours/OptimizingGreedy.js
+++ src/behaviours/OptimizingGreedy.js
@@ -26,6 +26,24 @@ const DIR = [
   {row: 0, col: -1}, // Directions.LEFT
 ];
 
+/**
+ * Algorithm Flow:
+ * - Find all the chains that can be completed, along with their length and
+ *   whether they are closed or half-open
+ * - If there are boxes with 3 or 4 unused lines, choose any line which
+ *   completes a box, regardless of the chain properties.
+ * - If there are no chains, select any line which doesn't complete a box.
+ * - At this point, all the boxes are missing either 1 or 2 lines.
+ * - If there is any chain longer than 4, select a line at its beginning.
+ * - If there is any open chain longer than 2, select a line at its beginning.
+ * - If there is any closed chain shorter than 4, complete it.
+ * - If there is any chain shorter than 1, complete it.
+ * - If there are multiple chains, select a line in the longest one.
+ * - At this point, we only have either a closed chain of length 4, or a
+ *   half-open chain of length 2, and all other lines start new chains.
+ * - Do alpha-beta pruning with one option being leaving the chain to the
+ *   opponent, and the other being getting the chain and
+ */
 export default class OptimizingGreedy extends SmartGreedy {
   run(gameState: GameState): ?Line {
     const rows = gameState.getRows();
