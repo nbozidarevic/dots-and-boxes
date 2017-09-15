362dac5fe7e01145d4b04172219ed1b5972f8ceb
nbozidarevic <nbozidarevic@gmail.com>
2017-09-14 23:16:36 -0700

    More logic for chains

--- src/behaviours/OptimizingGreedy.js
+++ src/behaviours/OptimizingGreedy.js
@@ -20,6 +20,16 @@ type StartingPoint = {
   value: number,
 };
 
+type Chain = {
+  row: number,
+  col: number,
+  length: number,
+}
+type Chains = {
+  closed: Array<Chain>,
+  open: Array<Chain>,
+};
+
 const DIR = [
   {row: -1, col: 0}, // Directions.UP
   {row: 0, col: 1}, // Directions.RIGHT
@@ -35,10 +45,10 @@ const DIR = [
  *   completes a box, regardless of the chain properties.
  * - [done] If there are no chains, select any line which doesn't complete a box.
  * - At this point, all the boxes are missing either 1 or 2 lines.
- * - If there is any chain longer than 4, select a line at its beginning.
- * - If there is any open chain longer than 2, select a line at its beginning.
- * - If there is any closed chain shorter than 4, complete it.
- * - If there is any chain shorter than 1, complete it.
+ * - [done] If there is any chain longer than 4, select a line at its beginning.
+ * - [done] If there is any open chain longer than 2, select a line at its beginning.
+ * - [done] If there is any closed chain shorter than 4, complete it.
+ * - [done] If there is any chain shorter than 2, complete it.
  * - If there are multiple chains, select a line in the longest one.
  * - At this point, we only have either a closed chain of length 4, or a
  *   half-open chain of length 2, and all other lines start new chains.
@@ -86,14 +96,32 @@ export default class OptimizingGreedy extends SmartGreedy {
     return counts;
   }
 
-  _getAvailableLineForBox(map: Map, row: number, col: number): ?Direction {
+  _getAvailableLineForBox(map: Map, row: number, col: number): Direction {
     const box = map[row][col];
     for (let dir = Directions.UP; dir <= Directions.LEFT; ++dir) {
       if (!box[dir]) {
         return dir;
       }
     }
-    return null;
+    throw Error('Expected to find an available line');
+  }
+
+  _getAllChains(map: Map): Chains {
+    const chains = {
+      closed: [],
+      open: [],
+    };
+
+    return chains;
+  }
+
+  _chainComparator(a: Chain, b: Chain) {
+    if (a.length > b.length) {
+      return -1;
+    } else if (a.length < b.length) {
+      return 1;
+    }
+    return 0;
   }
 
   run(gameState: GameState): ?Line {
@@ -101,15 +129,12 @@ export default class OptimizingGreedy extends SmartGreedy {
     const cols = gameState.getCols();
     const map = this._getMapFromGameState(gameState);
 
-    const boxesByLineCount =
-      this._getBoxesGroupedByAvailableLineCount(map);
-    console.log(boxesByLineCount);
-
     /**
      * If there are boxes with 3 or 4 unused lines, choose any line which
      * completes a box. If there is no such line, select any line which doesn't
      * complete a box.
      */
+    const boxesByLineCount = this._getBoxesGroupedByAvailableLineCount(map);
     if (boxesByLineCount[3].length + boxesByLineCount[4].length > 0) {
       let box;
       if (boxesByLineCount[1].length > 0) {
@@ -120,12 +145,69 @@ export default class OptimizingGreedy extends SmartGreedy {
           ...boxesByLineCount[4],
         ][0];
       }
-      const dir = this._getAvailableLineForBox(map, box.row, box.col);
-      if (dir === null || dir == undefined) {
-        throw Error('Expected to find an available line');
-      }
-      return gameState.getLine(box.row, box.col, dir);
+      return gameState.getLine(
+        box.row,
+        box.col,
+        this._getAvailableLineForBox(map, box.row, box.col),
+      );
     }
+
+    const chains = this._getAllChains(map);
+    chains.closed = chains.closed.sort(this._chainComparator);
+    chains.open = chains.open.sort(this._chainComparator);
+
+    if (chains.closed.length + chains.open.length > 0) {
+      let box;
+      if (chains.closed.length > 0 && chains.closed[0].length > 4) {
+        /**
+         * If there is any chain longer than 4, select a line at its beginning.
+         */
+        box = chains.closed[0];
+      } else if (chains.open.length > 0 && chains.open[0].length > 4) {
+        /**
+         * If there is any chain longer than 4, select a line at its beginning.
+         */
+        box = chains.open[0];
+      } else if (chains.open.length > 0 && chains.open[0].length > 2) {
+        /**
+         * If there is any open chain longer than 2, select a line at its
+         * beginning.
+         */
+        box = chains.open[0];
+      } else if (
+        chains.open.length > 0 &&
+        chains.open[chains.open.length - 1].length < 2
+      ) {
+        /**
+         * If there is any open chain shorter than 2, complete it.
+         */
+        box = chains.open[0];
+      } else if (
+        chains.closed.length > 0 &&
+        chains.closed[chains.closed.length - 1].length < 4
+      ) {
+        /**
+         * If there is any closed chain shorter than 4, complete it.
+         */
+        box = chains.closed[0];
+      }
+
+      if (box) {
+        return gameState.getLine(
+          box.row,
+          box.col,
+          this._getAvailableLineForBox(map, box.row, box.col),
+        );
+      }
+    }
+
+
+
+
+
+
+
+
 
     let startingPoints = [];
     for (let i = 0; i < rows; ++i) {
@@ -161,7 +243,7 @@ export default class OptimizingGreedy extends SmartGreedy {
 
   dfs(
     gameState: GameState,
-    map: Array<Array<SelectedLines>>,
+    map: Map,
     row: number,
     col: number,
     originDirection?: Direction,
